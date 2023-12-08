69c6acef23d4948ff514f3d1542e7919909c3b21
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2023-12-08 09:17:54 -0300

    feat[]: added static page for initial

--- back/app.js
+++ back/app.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const app = express()
 
+app.use(express.static('../front/'))
+
 app.get("/", (req, res) => {
-    res.send("ta funcionando")
+    res.sendFile('index.html')
 })
 
 app.listen(3000, () => {
