0c7cf05b75fe258017b8c695373f2d281f1327fb
melyssa08 <melyssa.rojas@sou.inteli.edu.br>
2023-12-09 05:54:55 -0300

    fix[]: added new block after post new list of formandos

---
+++ front/js/index.js
@@ -0,0 +1,27 @@
+var sequenceOfBlocks = document.getElementById("all-box");
+
+if (localStorage.getItem("respostaPlanilha")) {
+
+    var curso = localStorage.getItem("curso")
+    var data = localStorage.getItem("data")
+
+    var newBlock = document.createElement("div");
+
+    newBlock.textContent = `formandos-${curso}-${data}`;
+
+    newBlock.style.display = "flex";
+    newBlock.style.width = "25%";
+    newBlock.style.height = "200px";
+    newBlock.style.borderWidth = "3px";
+    newBlock.style.borderStyle = "solid";
+    newBlock.style.borderColor = "#1E709B";
+    newBlock.style.alignItems = "center";
+    newBlock.style.justifyContent = "center";
+    newBlock.style.flexDirection = "column";
+    newBlock.style.backgroundColor = "#1E709B"
+    newBlock.style.color = "#FFFFFF";
+    newBlock.style.fontFamily = "'Inter', sans-serif";
+    newBlock.style.marginLeft = "10px"
+
+    sequenceOfBlocks.appendChild(newBlock);
+}
--- front/js/register.js
+++ front/js/register.js
@@ -2,11 +2,11 @@ var buttonConfirm = document.getElementById("button-confirm");
 
 buttonConfirm.addEventListener("click", function () {
     var inputFile = document.getElementById("arquivoInput");
-    var label = document.getElementById("label-upload")
-    console.log(label.textContent)
+    var label = document.getElementById("label-upload");
+    var cursoInput = document.getElementById('curso')
+    var dataInput = document.getElementById('data')
+
     const arquivo = inputFile.files[0];
-    console.log("O QUE VEM");
-    console.log(arquivo);
 
     const formData = new FormData();
     formData.append('File', arquivo);
@@ -17,9 +17,11 @@ buttonConfirm.addEventListener("click", function () {
     })
     .then((response) => response.json())
     .then((dataOficial) => {
-        console.log("DEU CERTOOO");
-        console.log(dataOficial);
-        label.textContent = `${dataOficial}`
+        localStorage.setItem('respostaPlanilha', JSON.stringify(dataOficial));
+        localStorage.setItem('curso', cursoInput.value)
+        localStorage.setItem('data', dataInput.value)
+
+        window.location.href = '../index.html';
     })
     .catch((error) => {
         console.error("Erro:", error);
